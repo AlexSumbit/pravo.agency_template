15f95db82fe91eace7004b24abe8fe163c7e6d97
AlexSumbit <alexsumbit@gmail.com>
2018-01-21 15:45:51 +0200

    top section done, started section-services

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,6 +1,9 @@
 
-jQuery(document).ready(function($){
+$(document).ready(function($){
 
 
-
+    $(".js_btn-menu").click(function(){
+        $(".js_nav").toggleClass("nav-opened");
+        $(".js_btn-menu").toggleClass("btn-menu-opened");
+    });
 });
